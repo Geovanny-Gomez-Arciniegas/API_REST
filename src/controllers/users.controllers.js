310fcca91de889db540ff4c412130c0aed96e0c6
Geovanny-Gomez-Arciniegas <geovanny_gomeza@soy.sena.edu.co>
2024-05-30 09:45:10 -0500

    Se actualizan los mensajes de la API cuando no se encuentran usuarios, se instala un archivo que no se va a utilizar luego lo desistalare

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -5,6 +5,11 @@ import { pool } from '../db.js' // Importamos la conexión a la base de datos
 export const getUsers = async (req, res) => {
   // Consulta a la base de datos de todos los usuarios
   const { rows } = await pool.query('SELECT * FROM users')
+
+  if (rows.length === 0) {
+    return res.status(404).json('No hay usuarios registrados en la base de datos');
+  }
+
   res.json(rows);
 }
 // consultar un usuario por su id
@@ -24,15 +29,21 @@ export const getUserById = async (req, res) => {
 export const createUser = async (req, res) => {
   try {
     const data = req.body;
+    // Validamos que los campos name y email no estén vacíos
+    if (data.name === "" || data.email === "") {
+      return res.status(400).json({ message: 'name y email son requeridos' });
+    }
+
     const { rows } = await pool.query(
-    'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
-    [data.name, data.email]
+      'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
+      [data.name, data.email]
     );
     return res.json(rows[0]);
-    
+
   } catch (error) {
     console.log(error);
 
+    // Validamos si el error es por email duplicado
     if (error?.code === '23505') {
       return res.status(400).json('El email ya está en uso');
     }
@@ -43,22 +54,46 @@ export const createUser = async (req, res) => {
 // Eliminar un usuario
 export const deleteUser = async (req, res) => {
   const { id } = req.params;
+  // Consulta a la base de datos para eliminar un usuario por su id
 
-  const { rowCount } = await pool.query('DELETE FROM users WHERE id = $1 RETURNING *', [id]);
+  const { rows, rowCount } = await pool.query('DELETE FROM users WHERE id = $1 RETURNING *', [id]);
 
   if (rowCount === 0) {
     return res.status(404).json({ message: `El usuario con el id: ${id} no se ha encontrado` });
   }
 
-  return res.sendStatus(204);
+  return res.status(200).json({ message: `El usuario ${rows[0].name} se ha eliminado correctamente` });
+  // return res.status(404).json('No hay usuarios registrados en la base de datos');
+
 }
 // Actualizar un usuario
 export const updateUser = async (req, res) => {
-  const { id } = req.params;
-  const data = req.body;
 
-  const { rows } = await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *', [data.name, data.email, id]);
+  try {
+    const { id } = req.params;
+    const data = req.body;
 
-  return res.json(rows[0]);
+    if (data.name === "" || data.email === "") {
+      return res.status(400).json({ message: 'name y email son requeridos' });
+    }
+
+    const { rows } = await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *', [data.name, data.email, id]);
+  // Tener en cuenta evaluar si el usuario no existe
+    if (rows.length === 0) {
+      return res.status(404).json(`El Usuario con id: ${id} no se a encontrado`);
+    }
+
+    return res.json(rows[0]);
+
+
+  } catch (error) {
+    console.log(error);
+
+    if (error?.code === '23505') {
+      return res.status(400).json('El email ya está en uso');
+    }
+
+    return res.status(500).json('Internal Server Error');
+  }
 }
 
